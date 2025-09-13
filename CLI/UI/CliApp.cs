bb869f2120108c6578eee1e890b1d7c7690011e6
VicMarx1 <354405@via.dk>
2025-09-13 20:16:50 +0200

    Appet CLI

---
+++ CLI/UI/CliApp.cs
@@ -0,0 +1,11 @@
+﻿using CLI.UI;
+using InMemoryRepositories;
+using RepositoryContracts;
+
+Console.WriteLine("Starting CLI Application...");
+IUserRepository userRepository = new UserInMemoryRepository();
+CommentInMemoryRepository userCommentRepository = new CommentInMemoryRepository();
+IPostRepository postRepository = new PostInMemoryRepository();
+
+ICliApp cliApp = new CliApp(userRepository, userCommentRepository, postRepository);
+await cliApp.StartAsync();
