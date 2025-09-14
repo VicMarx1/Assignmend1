99b645c72f22a5213c0b55b0abb407822be286a4
Victor <354405@via.dk>
2025-09-14 22:52:36 +0200

    Option Switch

--- CLI/UI/CliApp.cs
+++ CLI/UI/CliApp.cs
@@ -1,6 +1,7 @@
 ﻿using CLI.UI.ManagePosts;
 using CLI.UI.ManageUsers;
 using RepositoryContracts;
+
 namespace CLI.UI;
 
 public class CliApp
@@ -9,15 +10,50 @@ public class CliApp
     private readonly ICommentRepository commentRepository;
     private readonly IPostRepository postRepository;
     private bool running;
-    
-    private ManagePostView managePostView;
-    private ManageUserView manageUserView;
 
-    public CliApp(IUserRepository userRepository, ICommentRepository commentRepository, IPostRepository postRepository)
+    private ManagePostsView managePostsView;
+    private ManageUsersView manageUsersView;
+
+    public CliApp(IUserRepository userRepository,
+        ICommentRepository commentRepository, IPostRepository postRepository)
     {
         this.userRepository = userRepository;
         this.commentRepository = commentRepository;
         this.postRepository = postRepository;
     }
-}
 
+    public async Task StartAsync()
+    {
+        running = true;
+        while (running)
+        {
+            Console.WriteLine("Select an option:");
+            Console.WriteLine("[1] Manage Posts \n" + "[2] Manage Users \n" + "[3] Exit");
+            int? selection = int.Parse(Console.ReadLine());
+            switch (selection)
+            {
+                case 1:
+                    if (managePostsView is null)
+                    {
+                        managePostsView = new ManagePostsView(postRepository,
+                            commentRepository, this);
+                    }
+
+                    await managePostsView.ShowOptions();
+                    break;
+                case 2:
+                    if (manageUsersView is null)
+                    {
+                        manageUsersView =
+                            new ManageUsersView(userRepository, this);
+                    }
+
+                    await manageUsersView.ShowOptions();
+                    break;
+                default:
+                    running = false;
+                    break;
+            }
+        }
+    }
+}
